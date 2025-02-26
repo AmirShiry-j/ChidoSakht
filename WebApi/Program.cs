30080892da9b0dc0063f83fa32b24130ebb08832
AmirShiry-j <AmirShiry06@gmail.com>
2025-02-26 22:51:42 +0330

    Set Swagger configs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,17 +1,79 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.OpenApi.Models;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Config controller service
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Config Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    //برای نمایش استرینگی Enum ها
+    //c.DescribeAllEnumsAsStrings();
+
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ChidoSakht", Version = "v1" });
+
+    //برای نمایش Description کنترلر ها
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "WebApi.ChidoSakht.xml"), true);
+
+    //For configure Authentication in swaager Ui
+    var security = new OpenApiSecurityScheme
+    {
+        Name = "JWT Auth",
+        Description = "توکن را وارد کنید",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(security.Reference.Id, security);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { security , new string[]{ } }
+                });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+//Swagger config
+app.UseSwagger(c =>
+{
+    c.SerializeAsV2 = false;
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1");
+    c.RoutePrefix = string.Empty;
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseDeveloperExceptionPage();
+}
+else
+if (app.Environment.IsProduction())
+{
+
 }
 
 app.UseHttpsRedirection();
