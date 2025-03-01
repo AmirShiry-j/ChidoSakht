ba09610000d615dc4a7823742de50c4e3309945b
AmirShiry-j <AmirShiry06@gmail.com>
2025-03-01 23:52:24 +0330

    Add a JwtInfo Struct

---
+++ WebApi/Helpers/JwtInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Helpers
+{
+    public struct JwtInfo
+    {
+        public static string Issuer = "ChidoSakht.ir";
+        public static string Audience = "ChidoSakht.ir";
+        public static string SecretKey = "{5CBAA322-1371-4756-A487-3F90A0DFB78C}";
+        public static DateTime Expires = DateTime.Now.AddDays(10);
+        public static DateTime NotBefore = DateTime.Now;
+        public static DateTime ExpiresRefreshToken = DateTime.Now.AddDays(30);
+    }
+}
