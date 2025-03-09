7a37b8d0c13319e9b8651cfbc5e1d7eac9ba289e
AmirShiry-j <AmirShiry06@gmail.com>
2025-03-10 00:47:16 +0330

    Define IdentitySettings to AppSettings

--- Application/ConfigService/IConfigService.cs
+++ Application/ConfigService/IConfigService.cs
@@ -14,6 +14,7 @@ namespace Application.ConfigService
     public class AppSettings
     {
         public DatabaseSettings DatabaseSettings { get; set; }
+        public IdentitySettings IdentitySettings { get; set; }  
         public EmailSetting EmailSetting { get; set; }
         public string[] CorsOrigins { get; set; }
         public Localization Localization { get; set; }
@@ -39,5 +40,16 @@ namespace Application.ConfigService
         public string DefaultCulture { get; set; } = "fa";
         public string[] SupportedCultures { get; set; } = { "fa" };
         public bool Visible_AcceptLanguageForSwagger { get; set; } = false;
+    }
+    public class IdentitySettings
+    {
+        public bool RequireDigit { get; set; }
+        public int RequiredLength { get; set; }
+        public bool RequireLowercase { get; set; }
+        public bool RequireUppercase { get; set; }
+        public bool RequireNonAlphanumeric { get; set; }
+        public int RequiredUniqueChars { get; set; }
+        public int MaxFailedAccessAttempts { get; set; }
+        public int DefaultLockoutTimeSpan    { get; set; }
     }
 }
