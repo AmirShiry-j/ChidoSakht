59b156832549f97f6e3f526a3b27542365f0a4da
AmirShiry-j <AmirShiry06@gmail.com>
2025-05-09 17:01:11 +0330

    Reforme product model

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -7,27 +7,63 @@ using System.Threading.Tasks;
 
 namespace Domain.Products
 {
+
     public class Product
     {
         public int Id { get; set; }
+        public string UniCode { get; set; } 
         public string Name { get; set; }
-        public string? Description { get; set; }
-        public string UniCode { get; set; }
-        public long Price { get; set; }
-        public long? SpecialPrice { get; set; }
-        public StatusInWarehouse StatusInWarehouse { get; set; }
-        public double Weight { get; set; }
-        public double Length { get; set; }
-        public double Width { get; set; }
-        public double Height { get; set; }
-        //Nav
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public string Description { get; set; }
+        public bool IsVariable { get; set; }
+
+        public ICollection<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
+        public ICollection<ProductAttribute> Attributes { get; set; } = new List<ProductAttribute>();
     }
-    public enum StatusInWarehouse
+
+
+    public class ProductVariant
     {
-        NonExistent,
-        Existent,
-        InAdvancePurchase
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public long Price { get; set; } 
+        public int Stock { get; set; }
+
+        public ICollection<VariantAttributeValue> AttributeValues { get; set; } = new List<VariantAttributeValue>();
     }
+
+
+    public class ProductAttribute
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public string Name { get; set; }
+        public ICollection<ProductAttributeValue> Values { get; set; } = new List<ProductAttributeValue>();
+    }
+
+    public class ProductAttributeValue
+    {
+        public int Id { get; set; }
+        public int AttributeId { get; set; }
+        public ProductAttribute Attribute { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    public class VariantAttributeValue
+    {
+        public int Id { get; set; }
+
+        public int ProductVariantId { get; set; }
+        public ProductVariant ProductVariant { get; set; }
+
+        public int AttributeValueId { get; set; }
+        public ProductAttributeValue AttributeValue { get; set; }
+    }
+
+
+
 }
