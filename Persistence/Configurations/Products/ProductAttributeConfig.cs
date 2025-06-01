4be2de831b0d49ffc49a71a64326287cc6020216
AmirShiry <AmirShiry06@gmail.com>
2025-06-01 15:43:38 +0330

    Unicode col be uniqe

---
+++ Persistence/Configurations/Products/ProductAttributeConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations.Products
+{
+    public class ProductAttributeConfig : IEntityTypeConfiguration<Domain.Products.ProductAttribute>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Products.ProductAttribute> builder)
+        {
+            var converterForAttributeTypeEnum = new Microsoft.EntityFrameworkCore.Storage.ValueConversion.EnumToStringConverter<Domain.Products.AttributeType>();
+
+
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.AttributeType).IsRequired().HasConversion(converterForAttributeTypeEnum);
+        }
+    }
+}
--- Persistence/Configurations/Products/ProductConfig.cs
+++ Persistence/Configurations/Products/ProductConfig.cs
@@ -17,19 +17,8 @@ namespace Persistence.Configurations.Products
             builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
             builder.Property(p => p.CreateTime).HasDefaultValueSql("getdate()");
             builder.HasIndex(p => p.UniqeLink).IsUnique();
+            builder.HasIndex(p => p.UniCode).IsUnique();
             //builder.Property(p=>p.ParentCategory).
-        }
-    }
-
-    public class ProductAttributeConfig : IEntityTypeConfiguration<Domain.Products.ProductAttribute>
-    {
-        public void Configure(EntityTypeBuilder<Domain.Products.ProductAttribute> builder)
-        {
-            var converterForAttributeTypeEnum = new Microsoft.EntityFrameworkCore.Storage.ValueConversion.EnumToStringConverter<Domain.Products.AttributeType>();
-
-
-            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
-            builder.Property(p => p.AttributeType).IsRequired().HasConversion(converterForAttributeTypeEnum);
         }
     }
 }
